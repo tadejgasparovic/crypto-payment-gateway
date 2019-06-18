db82062cebf385abe272537b28130544e1706535
tadejgasparovic <tadejgasparovic@gmail.com>
2019-06-18 11:55:09 +0200

    Updated dev oculor daemon

--- oculor-dev-daemon/up.sh
+++ oculor-dev-daemon/up.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run -ti -v /home/tadej/Upwork/crypto-payment-gateway/oculor-dev-daemon/oculor_data:/oculor/oculor_data -p 48844:48844 da6e5991015f $@
+docker build -t oculor docker/
+docker run -ti -v $(realpath ./oculor_data):/oculor/oculor_data -p 48844:48844 oculor $@
